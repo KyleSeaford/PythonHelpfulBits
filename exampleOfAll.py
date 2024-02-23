2bdbad1dfbe1a51b9c426f529f80c3c9de5824b5
KyleSeaford <kyleseaford5@gmail.com>
2024-02-23 17:58:01 +0000

    Remove unnecessary code for user input

--- exampleOfAll.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-
-name = pyautogui.prompt('Please enter your name:', default= 'Kyle Seaford')
-password = pyautogui.password('Enter your password:', default='password123')
-
-pyautogui.alert('Name: ' + name + '\nPassword: ' + password , 'Your Information', 'OK')
